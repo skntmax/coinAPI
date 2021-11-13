b230766b55102fe4ceead48cc3e9a0c2bd59a0a3
skntmax <skntjee@gmail.com>
2021-11-12 23:47:32 -0800

    fourth commit

--- frontend/usermsgnt/src/App.js
+++ frontend/usermsgnt/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './components/Homepage';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Logout from './components/Logout';
 function App() {
   return (
      <Switch>
 
      <Route exact path= "/" component={Homepage} /> 
      </Switch> 
-  
   );
 }
 
